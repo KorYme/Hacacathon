61fea32e24097dca94ea125055e516a57a915e8e
KorYme <maxime.corby.pro@gmail.com>
2023-11-15 14:23:34 +0100

    [ADD] : Three Questions + Clamp fontsize

--- BlindnessProject/Assets/Scripts/QCM_Handler.cs
+++ BlindnessProject/Assets/Scripts/QCM_Handler.cs
@@ -13,14 +13,36 @@ public class QCM_Handler : MonoBehaviour
 
     [SerializeField] float _initialFontSize;
     [SerializeField] float _sizeModifier;
+    [SerializeField] Vector2 _sizeClamp;
 
-    float _currentFontDelta = 0f;
-    int _questionCount = -2;
+    float _currentFontDelta;
+    int _questionCount;
+    int _numberOfAnswer;
+
+    private void Reset()
+    {
+        _initialFontSize = .5f;
+        _sizeModifier = .1f;
+        _sizeClamp = new Vector2(.1f, 1f);
+        _answersText = new List<TMP_Text>();
+        foreach (Transform child in transform)
+        {
+            if (child.name.Contains("Answer"))
+            {
+                _answersText.Add(child.GetComponent<TMP_Text>());
+            }
+            else
+            {
+                _questionText = child.GetComponent<TMP_Text>();
+            }
+        }
+    }
 
     private void Start()
     {
         _questionCount = -2;
         _currentFontDelta = 0f;
+        _numberOfAnswer = transform.childCount - 1;
         foreach (var question in _answersText)
         {
             question.fontSize = _initialFontSize + _currentFontDelta;
@@ -70,7 +92,7 @@ public class QCM_Handler : MonoBehaviour
 
         _questionCount++;
         _questionText.text = _questions[_questionCount].question;
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < _numberOfAnswer; i++)
         {
             _answersText[i].text = _questions[_questionCount].answers[i].AnswerData;
         }
@@ -78,17 +100,18 @@ public class QCM_Handler : MonoBehaviour
     
     public void CheckAnswer(int answerIndex)
     {
-        if (answerIndex >= 4) return;
+        if (answerIndex >= _numberOfAnswer) return;
         ChangeFontSize(_questions[_questionCount].answers[answerIndex].IsTheGoodAnswer);
         InitializeNewQuestion();
     }
 
     private void ChangeFontSize(bool isIncreasing)
     {
-        _currentFontDelta += _sizeModifier * (isIncreasing ? 1 : -1);
-        foreach (var question in _answersText)
+        _currentFontDelta = Mathf.Clamp(_currentFontDelta + _sizeModifier * (isIncreasing ? 1 : -1), _sizeClamp.x, _sizeClamp.y);
+        foreach (TMP_Text answer in _answersText)
         {
-            question.fontSize = _initialFontSize + _currentFontDelta;
+            answer.fontSize = _initialFontSize + _currentFontDelta;
         }
+        _questionText.fontSize = _initialFontSize + _currentFontDelta;
     }
 }
